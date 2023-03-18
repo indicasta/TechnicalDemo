6f9900f5002fafc6021bb7b6ed8728984389327e
IndiCasta <indicasta@gmail.com>
2023-03-18 13:48:40 +0100

    reordering file system

--- demo/src/main/java/com/slashmobility/demo/DemoApplication.java
+++ demo/src/main/java/com/slashmobility/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String hello(){
-		return String.format("Hello Beauty!");
+		return String.format("Hello World one more time!");
 	}
 
 
--- demo/src/main/java/com/slashmobility/demo/controllers/tShirtsController.java
+++ demo/src/main/java/com/slashmobility/demo/controllers/tShirtsController.java
@@ -1,4 +1,4 @@
-package com.slashmobility.controllers;
+package com.slashmobility.demo.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
